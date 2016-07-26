db372cd0cbbac45dda58cf2e8c2baf610a2c0508
Sujeet Kumar <sujeetkv90@gmail.com>
2016-07-26 12:45:58 +0530

    updated script

--- controllers/App_images.php
+++ controllers/App_images.php
@@ -3,7 +3,7 @@
 class App_images extends CI_Controller
 {	
 	public function index(){
-		// load library with config
+		// load library and initialize with config
 		$this->load->library('dynamic_image', array(
 			'cache_dir' => APPPATH.'cache/image_cache',
 			'create_sub_dir' => true,
@@ -23,14 +23,14 @@ class App_images extends CI_Controller
 		/* $this->dynamic_image->setWaterMark(array(
 			'smart_wm' => true,
 			'custom_config' => array(
-				'wm_text'			=> 'Sujeet',
-				'wm_type'			=> 'text',
-				'wm_font_path'		=> BASEPATH.'fonts/Bullpen3D.ttf',
-				'wm_font_size'		=> '12',
-				'wm_font_color'		=> '0066FF',
-				'wm_vrt_alignment'	=> 'top',
-				'wm_hor_alignment'	=> 'left',
-				'wm_padding'		=> '4'
+				'wm_text' => 'Sujeet',
+				'wm_type' => 'text',
+				'wm_font_path' => BASEPATH.'fonts/Bullpen3D.ttf',
+				'wm_font_size' => '12',
+				'wm_font_color' => '0066FF',
+				'wm_vrt_alignment' => 'top',
+				'wm_hor_alignment' => 'left',
+				'wm_padding' => '4'
 			)
 		)); */
 		
